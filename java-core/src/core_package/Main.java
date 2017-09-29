cb5fc1b3b05f448ff1d71af9606014b90739b0b8
Gayatri Krishnan <gayatrik97@gmail.com>
2017-09-29 15:11:59 -0700

    modified:   java-core/src/core_package/Main.java

--- java-core/src/core_package/Main.java
+++ java-core/src/core_package/Main.java
@@ -4,6 +4,19 @@ public class Main {
 	
 	public static void main(String args[]) {
 		
+		Table p = new Table("name", );
+		p.addAttribute("purchase_ID", true, ID);
+		Table cl = new Table("name", );
+		p.addAttribute("purchase_ID", true, ID);
+		
+		
+		
+		DBSchema sc = new DBSchema();
+		sc.addTable(p);
+		sc.addTable(cl);
+		sc.addRelationship(p,cl,"purchaseID", "clientID");
+		
+		Table featureTable = attributeGenerate (sc, p, "target-attribute");
 	}
 
 }
