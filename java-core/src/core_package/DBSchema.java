ca5ebc75b217fb5fbba1d13a23d9b5f4079f1eff
jackson <jhoagland@scu.edu>
2017-10-03 13:52:43 -0700

    modified:   java-core/src/core_package/DBSchema.java

--- java-core/src/core_package/DBSchema.java
+++ java-core/src/core_package/DBSchema.java
@@ -1,5 +1,13 @@
 package core_package;
 
+import java.util.ArrayList;
+
 public class DBSchema {
 
+	ArrayList<Table> tables;
+	ArrayList<Relationship> relationships;
+	
+	public DBSchema() {
+		tables.
+	}
 }
