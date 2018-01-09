b1016967ddad1aaddfa2475084618e02c3c5ea63
Michele Samorani <msamorani@scu.edu>
2018-01-09 11:12:19 -0800

    Now it works even without DB

--- java-core/src/core_package/Main.java
+++ java-core/src/core_package/Main.java
@@ -5,6 +5,7 @@ import core_package.QueryGeneration.Query;
 import core_package.QueryGeneration.QueryBuilder;
 import core_package.Schema.*;
 
+import java.time.Period;
 import java.util.ArrayList;
 
 
@@ -19,23 +20,29 @@ public class Main {
 	static ArrayList<Relationship> relationships = new ArrayList<>();
 	
 	public static void main (String [] args) throws Exception {
-
+	
 	    try {
             SchemaBuilder sb = new SchemaBuilder(DatabaseConnection.MICROSOFT_SQL_SERVER);
 					System.out.println(sb.buildSchema().getSchema().toString());
-        } catch (NoSuchDatabaseTypeException e) {
-	        e.printStackTrace();
-        }
 
         Schema sc = new Schema();
 	    sc.loadTables();
-
-		//loadTables();
+	    tables = sc.getTables();
+	    relationships = sc.getRelationships();
+        } catch (Exception e) {
+    		loadTables();
+        }
+	    
+	    System.out.println("********************");
+	    System.out.println("ATTRIBUTE GENERATION");
+	    System.out.println("********************");
 		JPL.init();
-		
+	       System.out.println("Working Directory = " +
+	               System.getProperty("user.dir"));
+	       
 		DB2PrologLoader.LoadDB(
 				"prolog/functions.pl",
-				sc.getTables(), sc.getRelationships());
+				tables, relationships);
 		ArrayList<Query> queries= QueryBuilder.buildQueriesFromDirectory("Purchases",
 		"prolog/query templates");
 		System.out.println("RESULT:");
@@ -45,4 +52,67 @@ public class Main {
 		return;
 	}
 
+	private static void loadTables() throws Exception {
+		Table purchases = new Table("Purchases");
+		purchases.addAttribute(new IDAttribute("Purchase_ID"));
+		purchases.setPrimaryKey(purchases.getAttributeByName("Purchase_ID"));
+		purchases.addAttribute(new IDAttribute("Client_ID"));
+		purchases.addAttribute(new IDAttribute("Product_ID"));
+		ArrayList<Period> periods = new ArrayList<>();
+		periods.add(Period.ofMonths(1));
+		periods.add(Period.ofMonths(2));
+		periods.add(Period.ofMonths(12));
+		purchases.addAttribute(new TimeStampAttribute("date", periods));
+		purchases.addAttribute(new ZeroOneAttribute("return"));
+		purchases.addAttribute(new ZeroOneAttribute("online"));
+
+		Table clients = new Table("Clients");
+		clients.addAttribute(new IDAttribute("Client_ID"));
+		clients.setPrimaryKey(clients.getAttributeByName("Client_ID"));
+		ArrayList<Double> binsAge = new ArrayList<>();
+		binsAge.add(0.0);
+		binsAge.add(20.0);
+		binsAge.add(30.0);
+		binsAge.add(40.0);
+		binsAge.add(50.0);
+		binsAge.add(60.0);
+		binsAge.add(100000.0);
+		clients.addAttribute(new NumericAttribute("age", "years", binsAge));
+		ArrayList<String> gvalues = new ArrayList<>();
+		gvalues.add("M");
+		gvalues.add("F");
+		clients.addAttribute(new NominalAttribute("gender", "gender", gvalues));
+
+		Table products = new Table("Products");
+		products.addAttribute(new IDAttribute("Product_ID"));
+		products.setPrimaryKey(products.getAttributeByName("Product_ID"));
+		ArrayList<Double> binsPrice = new ArrayList<>();
+		binsPrice.add(0.0);
+		binsPrice.add(20.0);
+		binsPrice.add(100.0);
+		binsPrice.add(200.0);
+		binsPrice.add(1000.0);
+		binsPrice.add(1000000.0);
+		products.addAttribute(new NumericAttribute("price", "dollars", binsPrice));
+
+		purchases.addRelationship(
+				new Relationship(purchases, clients, (IDAttribute) purchases.getAttributeByName("Client_ID"),
+						(IDAttribute) clients.getAttributeByName("Client_ID"), RelationshipType.To1));
+		clients.addRelationship(
+				new Relationship(clients, purchases, (IDAttribute) clients.getAttributeByName("Client_ID"),
+						(IDAttribute) purchases.getAttributeByName("Client_ID"), RelationshipType.ToN));
+		purchases.addRelationship(
+				new Relationship(purchases, products, (IDAttribute) purchases.getAttributeByName("Product_ID"),
+						(IDAttribute) products.getAttributeByName("Product_ID"), RelationshipType.To1));
+		products.addRelationship(
+				new Relationship(products, purchases, (IDAttribute) products.getAttributeByName("Product_ID"),
+						(IDAttribute) purchases.getAttributeByName("Product_ID"), RelationshipType.ToN));
+
+		tables.add(purchases);
+		tables.add(clients);
+		tables.add(products);
+		for (Table t : tables)
+			for (Relationship r : t.getRelationships())
+				relationships.add(r);
+	}
 }
